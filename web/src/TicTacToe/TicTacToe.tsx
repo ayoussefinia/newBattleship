b90d925b50f1b01f4c752c738de75a661526e56c
David H <drhinote@gmail.com>
2022-04-12 17:13:58 -0500

    Running

--- web/src/TicTacToe/TicTacToe.tsx
+++ web/src/TicTacToe/TicTacToe.tsx
@@ -3,16 +3,12 @@ import React, { useEffect, useRef} from "react";
 
 function Game( props: any) {
     
-    useEffect(() => {
-       props.exit();
-    });
-
-    return (<Board width={128} height={128}/>);
+    return (<div><Board width={1000} height={1000}/><button onClick={props.exit}>Leave</button></div>);
 }
 
 function TicTacToe(this: any, exit: any) {
     this.game = <Game exit={exit} />;
-    this.thumbnail = <div><h3>Tic Tac Toe</h3><Board width={128} height={128}/></div>;
+    this.thumbnail = <div><h3>Tic Tac Toe</h3><Board width={256} height={200}/></div>;
     this.name = 'Tic Tac Toe';
 }
 
