f38f0c297bd78191fbdf42855b4c1e2183111825
David H <drhinote@gmail.com>
2022-04-14 19:03:06 -0500

    More API work, client side api consumption example

---
+++ api/Connections.js
@@ -0,0 +1,106 @@
+const { WaitingRoom } = require("./WaitingRoom");
+const { PubSub } = require('graphql-subscriptions');
+
+const pubSub = new PubSub();
+
+const expired = { 
+
+};
+const waiting = {
+
+};
+const pings = {
+
+};
+
+function expireId(playerId) {
+    expired[playerId] = pings[playerId];
+    delete pings[playerId];
+}
+
+function signalDisconnect(playerId) {
+    if(expired[playerId] && expired[playerId] != "none") pubsub.publish(expired[playerId], { error: "Other player disconnected" });
+}
+
+function isPlayerIdExpired(playerId) {
+    return !pings[playerId] && !expired[playerId];
+}
+
+function checkForResponse(playerId, pong) {
+    if(!pings[playerId]) { 
+       signalDisconnect(playerId);
+    } 
+    else {
+       expireId(playerId);
+        pong();
+     }
+}
+
+function setPings(playerId, opponentId) {
+    if(isPlayerIdExpired(opponentId)) {
+        signalDisconnect(playerId);
+    } else {
+        pings[playerId] = opponentId || none;
+        delete expired[playerId];
+    }
+}
+
+
+const openSocket = (game, playerId) => {
+    function ping() { 
+        this.pong = () => { setTimeout(() => checkForResponse(playerId, this.pong), 15000);  };
+        this.pong();
+    }
+
+    const player1 = waiting[game];
+    const player2 = playerId;
+    
+    if(!player1) {
+        waiting[game] = player2;
+        setPings(player2, "none");
+    } else {
+        delete waiting[game];
+        setPings(player2, player1);
+        startMatch(player1, player2);
+    }
+}; 
+
+const startMatch = (player1, player2) => {
+    player1.opponentId = player2.playerId;
+    player2.opponentId = player1.playerId;
+
+    pubsub.publish(player1.playerId, { playerId: player1.playerId, opponentId: player2.playerId, data: { start: true }});
+};
+
+const takeTurn = (msg, playerId) => {
+    msg.playerId = msg.opponentId;
+    msg.opponentId = playerId;
+    if(msg.data.leaving) { 
+        expireId(playerId);
+        signalDisconnect(playerId);
+    } else {
+        pubsub.publish(msg.playerId, msg);
+    }
+};
+
+/* {
+    playerId,
+    opponentId,
+    data: { 
+        // custom data object
+     }
+}  */
+
+exports.Connections = {
+    playerConnected: (msg) => {
+        const playerId = uuidv4();
+        msg.playerId = playerId;
+        openSocket(msg.game, msg.playerId);
+        return pubsub.asyncIterator(msg.playerId);        
+    },
+    incomingMessage: (msg) => {
+        if(!msg.playerId) return;
+        setPings(msg.playerId, msg.opponentId);
+        if(msg.data) takeTurn(msg, msg.playerId);
+    }
+};
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,38 +6,28 @@ import Board from './TicTacToe/board'
 import TicTacToe from './TicTacToe/TicTacToe';
 import Home from './Home';
 
-const QUEUE_PLAYER = gql`
-  mutation Mutation($game: String!) {
-    queuePlayer(game: $game)
-  }    
-  `;
-
-const GET_STATUS = gql`
-  query ExampleQuery {
-    getStatus {
-      game
-    }
-  }
-  `;
 
 function App() {
 
 const [page, setPage] = useState(null);
 const exit = () => setPage(null);
+
+/// ===========================================  Add Games Here
 const pages = [ 
   new (TicTacToe as any)(exit),
 
 ];
+// =========================================================
 
 const home = <Home pages={pages} setPage={setPage} />;
 
-  //const [startGame, { data, loading, error }] = useMutation(QUEUE_PLAYER);
+  //
    //const { data, loading, error } = useQuery(GET_STATUS);
 
    //if (loading) return <p>'Submitting...'</p>;
    //if (error) return <p>`Submission error! ${error.message}`</p>;
 
-  return page||home;
+  return page || home;
 }
 
 export default App;
