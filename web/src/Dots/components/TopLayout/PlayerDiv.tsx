3023f0af5aa400f22c9cad89bfb085687cc5ec97
Jacob <jacobjames1117@gmail.com>
2022-04-23 14:30:28 -0600

    added the color of the player name to pass along in the props to the player div

--- web/src/Dots/components/TopLayout/PlayerDiv.tsx
+++ web/src/Dots/components/TopLayout/PlayerDiv.tsx
@@ -1,9 +1,14 @@
 import "./PlayerDiv.css";
 
 const PlayerDiv = (props: any) => {
+
+const PlayerStyle= {
+  color: props.PlayerColor,
+}
+
   return (
     <div className="player-div">
-      <label className="player-name">{props.PlayerName}</label>
+      <label className="player-name" style={PlayerStyle}>{props.PlayerName}</label>
       <div className="score">{props.TheScore}</div>
       <label className="score-label">Score</label>
       <label className="emoji">{props.Emoji}</label>
