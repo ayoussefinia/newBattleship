109b3c77e0b6da6efc8cfebab041bc7c4918c376
Ehsanul Muhit <emuhit786@gmail.com>
2022-05-01 12:16:21 -0400

    Added Matching Game Logic

--- web/src/MatchingGame/SingleCard/SingleCard.js
+++ web/src/MatchingGame/SingleCard/SingleCard.js
@@ -1,12 +1,27 @@
+import { CellTower } from "@mui/icons-material";
 import React, { useState } from "react";
 import "./SingleCard.css";
 
-export default function SingleCard( {card} ) {
+export default function SingleCard({ card, clickReveal, disabled, flipped }) {
 
+  function handleClick() {
+    if (!disabled) {
+      clickReveal(card)
+    }
+  }
+  if (card.matched) {
+    return <div style={{ backgroundColor: "white", border: "white" }} className="card"><div className="matched"></div></div>
+  }
+  if (flipped) {
+    return (
+      <div style={{ backgroundColor: "lightgrey" }}  className="card">
+        <div className="flipped">
+          {card.value}
+        </div>
+      </div>
+    )
+  } else {
+    return <div className="card" onClick={handleClick} style={{ backgroundColor: "red" }}><div className="hidden">?</div></div>
+  }
 
-  return (
-    <div>
-      {card}
-    </div>
-  )
 }
